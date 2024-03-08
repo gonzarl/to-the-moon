41e68b02f6935b644f868523b06427e11304a760
gonzarl <gonzariquelme66@gmail.com>
2024-03-08 16:59:37 -0300

    Added Darkmode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://to-the-moon-nashe.netlify.app/'
+  site: 'https://to-the-moon-nashe.netlify.app/',
+  integrations: [preact()]
 });
